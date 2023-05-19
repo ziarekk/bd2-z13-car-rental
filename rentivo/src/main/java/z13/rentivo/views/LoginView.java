98b037ee8d6cee5973a5f34494ecd6c334091ea5
jkierucz <01169183@pw.edu.pl>
2023-05-19 23:37:21 +0200

    Added simple Login View

---
+++ rentivo/src/main/java/z13/rentivo/views/LoginView.java
@@ -0,0 +1,57 @@
+package z13.rentivo.views;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+
+@Route("login") 
+@PageTitle("Login | Rentivo")
+@AnonymousAllowed
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+	private final LoginForm login = new LoginForm(); 
+
+	Dialog dialogMail = new Dialog();
+
+	public LoginView() {
+
+		addClassName("login-view");
+		setSizeFull(); 
+		setAlignItems(Alignment.CENTER);
+		setJustifyContentMode(JustifyContentMode.CENTER);
+
+		login.setAction("login"); 
+		
+		login.addForgotPasswordListener(e-> {
+			Notification.show("Forgot password clicked");
+        });
+
+		add(new H1("Rentivo"), login);
+	}
+
+	@Override
+	public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+		// inform the user about an authentication error
+		if(beforeEnterEvent.getLocation()  
+        .getQueryParameters()
+        .getParameters()
+        .containsKey("error")) {
+            login.setError(true);
+        }
+	}
+
+}
+
